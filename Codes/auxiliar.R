21d3c4574bcd990668f871dac59046debbb18a23
jairpaulino <63611561+jairpaulino@users.noreply.github.com>
2021-01-12 14:21:50 -0300

    20200112

--- Codes/auxiliar.R
+++ Codes/auxiliar.R
@@ -31,15 +31,34 @@ normalize = function(x) {
 }
 
 # Normalize functions
-normalize_2 = function(array, x, y, max, min){
+normalize_2 = function(array, x = 0.2, y = 0.8){
   #Normalize to [0, 1]
-  m = min
-  range = max - m
+  m = min(array)
+  range = max(array) - m
   norm1 = (array - m) / range
   
   #Then scale to [x,y]
   range2 = y - x
   normalized = (norm1*range2) + x
   return(normalized)
+}
+
+getMetrics = function(y_pred, y_true){
+  #y_pred = pred_CBG; y_true = dataNormTest$class
+  
+  acc = Accuracy(y_pred, y_true)
+  erroRate = 1 - acc
+  recall = Recall(y_pred, y_true)
+  f1Score = F1_Score(y_pred, y_true)
+  precision = Precision(y_pred, y_true)
+  
+  metrics_df = as.data.frame(matrix(ncol=4, nrow=1))           
+  names(metrics_df) = c('ErroRate', 'Precision', 'recall', 'F1') 
+  metrics_df$ErroRate = erroRate
+  metrics_df$Precision = precisao
+  metrics_df$recall = recall
+  metrics_df$F1 = f1Score
+  
+  return(metrics_df)
 }
 
--- Codes/q2_main.R
+++ Codes/q2_main.R
@@ -3,13 +3,17 @@
 rm(list = ls()); graphics.off()
 
 # Bibliotecas
-library(GGally)   #ggpairs
-library(ggplot2)  #ggplot
-library(corrplot) #cor
-library(caTools)  #sample.split
-library(e1071)    #naiveBayes
-library(caret)    #confusionMatrix
- 
+library(GGally)    #ggpairs
+library(ggplot2)   #ggplot
+library(corrplot)  #cor
+library(caTools)   #sample.split
+library(e1071)     #naiveBayes
+library(caret)     #confusionMatrix
+library(MLmetrics)
+library(stargazer) #stargazer
+library(faraway)   #vif
+library(glmnet)    #cv.glmnet
+
 # Chamar funcoes
 source("Codes/auxiliar.R")
 
@@ -21,35 +25,26 @@ data$class = factor(data$class, levels = c(0, 1))
 
 # Descricao dos dados
 summary(data)
-pairs(data[,1:4], diag.panel = panel.hist)
-pairs(data[,1:4], diag.panel = panel.hist, 
-      upper.panel = panel.cor)
-pairs(data[,1:4], diag.panel = panel.hist, 
-      upper.panel = panel.cor,
-      lower.panel = panel.lm)
-
-ggpairs(data[,1:4], lower = list(continuous = "smooth"))
+ 
+# ggpairs(data[,1:4], lower = list(continuous = "smooth"))
 ggpairs(data, columns = 1:4, ggplot2::aes(colour=class))
 ggcorr(data, label=T)
 
-M <- cor(data[1:4])
-corrplot(M, method = "circle", type = "upper")
+# M <- cor(data[1:4])
+# corrplot(M, method = "circle", type = "upper")
 
 # Normalizar
-dataNorm = as.data.frame(sapply(data[,1:4], FUN = normalize))
+dataNorm = as.data.frame(sapply(data[,1:4], FUN = normalize_2))
 dataNorm$class = data$class
 
 # Separar em conjunto de treinamento e teste
 sample = sample.split(dataNorm$vwti, SplitRatio = 0.8)
 dataNormTrain = subset(dataNorm, sample == T)
 dataNormTest = subset(dataNorm, sample == F)
-#perc = 0.8
-#dataNormTrain = dataNorm[1:round(perc*length(data$vwti)),]
-#dataNormTest = dataNorm[(round(perc*length(data$vwti))+1):length(data$vwti),]
 
 # Fase 2 - Modelagem ----
 
-# M1 - Classificador bayesiano gaussiano (CBG)
+# M1 - Classificador bayesiano gaussiano (CBG) ----
 CBG = naiveBayes(x = dataNormTrain[-5], y = dataNormTrain$class)
 print(CBG)
 
@@ -58,6 +53,96 @@ pred_CBG = predict(CBG, newdata = dataNormTest[-5])
 matriz_CBG = table(dataNormTest$class, pred_CBG)
 
 confusionMatrix(matriz_CBG, positive = "1")
+
+cbgMetrics = getMetrics(pred_CBG, dataNormTest$class)
+write.csv(cbgMetrics, file = "Results/metrics_cbg.csv")
+
+# M2 - Classificador bayesiano gaussiano baseado em K-vizinhos (CB-kV)
+# M3 - Classificador bayesiano gaussiano baseado em Janela de Parzen (CB-JP). 
+# M4 - Regressão Logística (RL) ----
+
+RL = glm(class ~ ., 
+         data = dataNormTrain,
+         family = binomial(link = 'logit')
+         )
+
+summary(RL)
+stargazer(RL, title = "Resultados", type = "text")
+
+pred_RL = predict(RL, newdata = dataNormTest[-5], type = "response")
+#View(pred_RL)
+plot(pred_RL)
+
+# analises extras 
+step(RL, direction = "both") #stepWise
+vif(RL) #VIF
+
+pred_RL = ifelse(pred_RL < 0.5, 0, 1)
+
+matriz_RL = table(dataNormTest$class, RL_pred)
+
+confusionMatrix(matriz_RL, positive = "1")
+
+rlMetrics = getMetrics(pred_RL, dataNormTest$class)
+write.csv(rlMetrics, file = "Results/metrics_rl.csv")
+
+# M5 - Regressão logistica com Regularizacao (RLR) ----
+# M6 - Ensemble com regra do voto majoritario (ERVM)
+
+data_lr = dataNorm
+data_lr$x5 = (dataNorm$vwti)^2
+data_lr$x6 = sqrt(dataNorm$swti) 
+data_lr$x7 = log(dataNorm$cwti) 
+data_lr$x8 = (dataNorm$ei)^(-1) 
+
+lr_reg_ind = data_lr[,c(1:4,6:9)]
+lr_reg_dep = data_lr$class
+set.seed(123) 
+
+cvLR = cv.glmnet(x = as.matrix(lr_reg_ind),
+                  y = lr_reg_dep,
+                  family = "binomial",
+                  nfolds = 10,
+                  alpha = 1,
+                  nlambda = 100,
+                  parallel = T)
+
+model.cv
+plot(model.cv)
+
+pred_RL_reg = predict.glmnet(cvLR, 
+                             newx = lr_reg_ind, 
+                             type = "response")
+
+rl_regMetrics = getMetrics(pred_RL, dataNormTest$class)
+write.csv(rlMetrics, file = "Results/metrics_rl_reg.csv")
+
+
+# RL_reg = glm(class ~ ., 
+#          data = data_lr,
+#          family = binomial(link = 'logit')
+# )
+# 
+# summary(RL_reg)
+# stargazer(RL_reg, title = "Resultados", type = "text")
+# 
+# pred_RL_reg = predict(RL_reg, newdata = data_lr[-5], type = "response")
+# #View(pred_RL)
+# plot(pred_RL_reg)
+
+# analises extras 
+# step(RL_reg, direction = "both") #stepWise
+# vif(RL_reg) #VIF
+# 
+# pred_RL_reg = ifelse(pred_RL_reg < 0.5, 0, 1)
+# 
+# matriz_RL_reg = table(data_lr$class, pred_RL_reg)
+# 
+# confusionMatrix(matriz_RL_reg, positive = "1")
+# 
+# rlreg_Metrics = getMetrics(pred_RL_reg, data_lr$class)
+# write.csv(rlreg_Metrics, file = "Results/metrics_rl_reg.csv")
+
 
 # Fase 3 - Teste ----
 

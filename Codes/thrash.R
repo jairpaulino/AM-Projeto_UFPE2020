a2b6b27d304c3dbae945f83e50b0b27dddd97cff
jairpaulino <63611561+jairpaulino@users.noreply.github.com>
2021-01-26 16:09:00 -0300

    Update thrash.R

--- Codes/thrash.R
+++ Codes/thrash.R
@@ -36,3 +36,71 @@ modelP_1 = mkde(as.matrix(hum_X),
                 thumb = 'none')
 
 #View(modelP_0); View(modelP_1)
+library("MASS")
+data("birthwt")
+attach(birthwt)
+library(np)
+
+data("Italy")
+attach(Italy)
+# First, compute the bandwidths... note that this may take a minute or
+# two depending on the speed of your computer.
+bw <- npcdensbw(formula=gdp~ordered(year))
+# Next, compute the condensity object...
+fhat <- npcdens(bws=bw)
+# The object fhat now contains results such as the estimated conditional
+# density function (fhat$condens) and so on...
+summary(fhat)
+# Call the plot() function to visualize the results (<ctrl>-C will
+# interrupt on *NIX systems, <esc> will interrupt on MS Windows
+# systems).
+plot(bw)
+detach(Italy)
+# EXAMPLE 1 (INTERFACE=DATA FRAME): For this example, we load Giovanni
+# Baiocchi's Italian GDP panel (see Italy for details), and compute the
+# likelihood cross-validated bandwidths (default) using a second-order
+# Gaussian kernel (default). Note - this may take a minute or two
+# depending on the speed of your computer.
+data("Italy")
+attach(Italy)
+# First, compute the bandwidths... note that this may take a minute or
+# two depending on the speed of your computer.
+# Note - we cast `X' and `y' as data frames so that plot() can
+# automatically grab names (this looks like overkill, but in
+# multivariate settings you would do this anyway, so may as well get in
+# the habit).
+X <- data.frame(year=ordered(year))
+y <- data.frame(gdp)
+bw <- npcdensbw(xdat=X, ydat=y)
+# Next, compute the condensity object...
+fhat <- npcdens(bws=bw)
+# The object fhat now contains results such as the estimated conditional
+# density function (fhat$condens) and so on...
+summary(fhat)
+# Call the plot() function to visualize the results (<ctrl>-C will
+# interrupt on *NIX systems, <esc> will interrupt on MS Windows systems).
+plot(bw)
+detach(Italy)
+# EXAMPLE 2 (INTERFACE=FORMULA): For this example, we load the old
+# faithful geyser data from the R `datasets' library and compute the
+# conditional density function.
+library("datasets")
+data("faithful")
+attach(faithful)
+# Note - this may take a few minutes depending on the speed of your
+# computer...
+bw <- npcdensbw(formula=eruptions~waiting)
+summary(bw)
+# Plot the density function (<ctrl>-C will interrupt on *NIX systems,
+# <esc> will interrupt on MS Windows systems).
+plot(bw)
+detach(faithful)
+# EXAMPLE 2 (INTERFACE=DATA FRAME): For this example, we load the old
+# faithful geyser data from the R `datasets' library and compute the
+# conditional density function.
+library("datasets")
+data("faithful")
+attach(faithful)
+# Note - this may take a few minutes depending on the speed of your
+# computer...
+# Note - we cast `X' and `y' as data frames so that plot() can
